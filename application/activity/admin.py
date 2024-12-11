24482d5c7a33a71a5a08689e0ff1b96edf951cbe
Alex Bian <22373017@buaa.edu.cn>
2024-12-11 23:06:10 +0800

    更新文档 && 优化后台显示

--- application/activity/admin.py
+++ application/activity/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from application.activity.models import Activity
+from application.activity.models.activity_user_relationship import ActivityUserRelationship
 
 # Register your models here.
 admin.site.register(Activity)
+admin.site.register(ActivityUserRelationship)
--- application/task/admin.py
+++ application/task/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from application.task.models import Task
+from application.task.models import TaskUserRelationship
 
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(TaskUserRelationship)
